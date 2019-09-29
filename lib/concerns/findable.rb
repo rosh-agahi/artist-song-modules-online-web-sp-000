2e77643eaa30e636f7f77272ec2b897089844a2c
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-29 18:43:40 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,9 +2,9 @@ module Findable
   
   module ClassMethods
   
-    def find_by_name 
-      self.class.all.detect{|a| a.name == name}
-    end 
+  def self.find_by_name(name)
+    self.all.detect{|a| a.name == name}
+  end
     
   end
 
